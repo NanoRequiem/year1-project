88b39a9a9c6f3110c0b219d2ebb2ad2b683a34d9
Brooklyn Mcswiney <ed20b3m@leeds.ac.uk>
2022-03-01 15:56:56 +0000

    Implemented validation check for cmd line arguments

--- assignment_1/pgmUtilities/pgmEcho.c
+++ assignment_1/pgmUtilities/pgmEcho.c
@@ -2,5 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
-// 
-// 
+
+
+
+//Main method for reading in cmd line arguments
+//
+//argc = number of command line arguments
+//**argv = list of all command line arguments
+
+int main(int argc, char **argv)
+{
+	//Call method to check the number of cmd line arguements
+	//if returns false error the program and exit
+	if(validateCmdArguments(3, argc))
+	{
+		printf("ERROR: Bad argument count");
+		return 1;
+	}
+} 
