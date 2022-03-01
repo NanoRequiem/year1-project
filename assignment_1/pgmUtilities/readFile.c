2195f0477be11800793ac6fb95f755a24072a97c
Brooklyn Mcswiney <ed20b3m@leeds.ac.uk>
2022-03-01 16:06:54 +0000

    initialized readFile.c and added functionality to validate cmd line arguments

---
+++ assignment_1/pgmUtilities/readFile.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+//Method to check the cmd line inputs
+//
+//requiredArguments - The arguments required for the utility
+//argc - The actual number of arguments inputted by the user
+
+int validateCmdArguments(int requiredArguments, int argc)
+{
+	if(requiredArguments != argc)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
