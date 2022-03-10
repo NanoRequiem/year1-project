78cd9a132c02cb682a9e324afead202fe4580dbe
Brooklyn Mcswiney <ed20b3m@leeds.ac.uk>
2022-03-10 16:20:26 +0000

    changed test.sh functionality to use makefile instead of typing gcc manually

--- assignment_1/pgmUtilities/test.sh
+++ assignment_1/pgmUtilities/test.sh
@@ -1,11 +1,14 @@
- gcc pgmEcho.c readFile.c outputFile.c -std=c99 -o pgmEchoTest
- ./pgmEchoTest identical.pgm temp1.pgm
+ echo "Making executables.."
+ make all
+
+ echo "testing successful pgmEcho..."
+ ./pgmEcho identical.pgm temp1.pgm
  echo "Openining test output..."
  more temp1.pgm
- rm pgmEchoTest
  rm temp1.pgm
 
- echo "testing pgmComp..."
- gcc pgmComp.c readFile.c -std=c99 -o pgmCompTest
- ./pgmCompTest slice0a.pgm identical.pgm
- rm pgmCompTest
+ echo "testing successful pgmComp..."
+ ./pgmComp slice0a.pgm identical.pgm
+
+ echo "removing executables"
+ make clean
