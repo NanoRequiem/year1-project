75ebd87462e992e65a6e4c6842a8896e9d88f422
Brooklyn Mcswiney <ed20b3m@leeds.ac.uk>
2022-03-13 17:36:21 +0000

    created files for outputting a binary file

---
+++ assignment_1/pgmUtilities/outputBinary.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "imageStructures.h"
+
+//outputs binary file given image data and output file path
+int outputBinaryFile(Image *inputImage, char *outFile)
+{
+  return 0;
+}
---
+++ assignment_1/pgmUtilities/outputBinary.h
@@ -0,0 +1,4 @@
+//header file for the module to output binary files
+
+//outputs binary file given image data and output file path
+int outputBinaryFile(Image *inputImage, char *outFile);
