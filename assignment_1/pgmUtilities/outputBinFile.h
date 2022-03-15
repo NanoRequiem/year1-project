98bdad3ba8caae8c0629738d20e26ff8ada5335e
Brooklyn Mcswiney <ed20b3m@leeds.ac.uk>
2022-03-15 19:49:42 +0000

    removed unneccesary files

--- assignment_1/pgmUtilities/outputBinFile.h
+++
@@ -1,6 +0,0 @@
-//Header file for outputting binary files
-//outputbinary function that outputs file
-//outImage = struct that stores image data for the 
-//outputFile = path for the output file
-//Returns an int signifying if it was successful or not
-int outputBinary(Image *inputImage, char *outFile);
