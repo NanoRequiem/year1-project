dc1ea0db82676c6bf85a636daa658a21cdf9c1a2
Brooklyn Mcswiney <ed20b3m@leeds.ac.uk>
2022-03-16 10:46:43 +0000

    Removed pgmUtilities directory

--- assignment_1/pgmUtilities/pgmEcho.h
+++
@@ -1,6 +0,0 @@
-
-//Header file for the pgmEcho program
-//
-//Main method to read in cmd line arguments
-
-int main(int argc, char **argv);
--- assignment_1/pgmUtilities/pgma2b.h
+++
@@ -1,5 +0,0 @@
-//Header file for pgma2b
-//
-//Main method to read in cmd line arguments
-
-int main(int argc, char **argv);
