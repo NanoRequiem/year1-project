9f69a4b37878ba96c77a5b09fbeb85f816d108d8
Brooklyn Mcswiney <ed20b3m@leeds.ac.uk>
2022-03-17 16:43:23 +0000

    added commenting to imageStructures.h

--- assignment_1/imageStructures.h
+++ assignment_1/imageStructures.h
@@ -1,5 +1,12 @@
 //Contains the structures to store image files
 
+//magicNumber = Magic number of the pgm file either P5 or P2
+//magic_Numer = Raw version of the magic number
+//commentLine = CommentLine indicated by #
+//width, height = Dimensions of the pgm file
+//maxGray = The maximum gray value of the pgm file
+//imageData = 2d array to store the data of the pgm file
+//rawImageData = 1d array to store the data of the pgm file
 typedef struct
 {
   char magicNumber[2];
