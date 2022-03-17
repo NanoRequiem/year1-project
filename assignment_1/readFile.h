bd244e3ba2a70e86e745e4b30e015e113270c0a7
Brooklyn Mcswiney <ed20b3m@leeds.ac.uk>
2022-03-17 16:17:39 +0000

    added commenting to readFile.h

--- assignment_1/readFile.h
+++ assignment_1/readFile.h
@@ -1,15 +1,36 @@
 
 //Header file for the readFile.c program
+//Brooklyn Mcswiney
 //
+
 //validateCmdArguments validates the command line arguments
+//requiredArguments = Number of command line arguments needed
+//argc = Number of command line arguments specified
 int validateCmdArguments(int requiredArguments, int argc);
 
+//Method to initialize the image struct
+//inputImage = struct to save all the image data to
 int initImage(Image *inputImage);
 
+//method to read in the header data to the image struct
+//inputImage = struct to save all the image data to
+//data = file path to the opened image
 int readImageHead(Image *inputImage, FILE *data);
-int readASCIIData(Image *inputImage, FILE *data);
-int saveRAWData(Image *InputImage);
-int ReadRAWData(Image *inputImage, FILE *data);
-int rawDataToImageData(Image *inputImage);
 
-int freeData(Image *inputImage);
+//method to read in the values for the image should it be an ASCII file
+//inputImage = struct to save all the image data to
+//data = file path to the opened image
+int readASCIIData(Image *inputImage, FILE *data);
+
+//Method to convert the 2d list to a 1d list in case the ASCII data needs to be outputted as a raw image
+//inputImage = struct to save all the image data to
+int saveRAWData(Image *InputImage);
+
+//Method to read in values for the image should it be a binary file
+//inputImage = struct to save all the image data to
+//data = file path to the opened image
+int ReadRAWData(Image *inputImage, FILE *data);
+
+//Method to convert the 1d list into a 2d list so that other modules can use the data
+//inputImage = struct to save all the image data to
+int rawDataToImageData(Image *inputImage);
